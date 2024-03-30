6f8e6d95c88584220cee717f85a07dca22407777
Regina Ofori-Boateng <52751600+reginaofori@users.noreply.github.com>
2024-03-30 16:45:49 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adversarial-noise-generator',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchvision',
+        'Pillow',
+
+        
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=6.0.0',
+        ]
+    },
+    python_requires='>=3.6',
+
+    entry_points={
+    'console_scripts': [
+        'generate-adversarial=adversarial.generate_examples:main',
+    ],
+},
+
+)
